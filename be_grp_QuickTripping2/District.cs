0f281f4187a8745527ac6e9cb34abb2397449c56
Martin Sisk <Brothermj@hotmail.com>
2021-08-10 20:25:31 -0500

    Added a static List<District> in a DistrictRepository class. Also defined DistrictName property.

--- be_grp_QuickTripping2/District.cs
+++ be_grp_QuickTripping2/District.cs
@@ -6,12 +6,29 @@ using System.Threading.Tasks;
 
 namespace be_grp_QuikTrippin
 {
+    class DistrictRepository
+    {
+        static List<District> district = new List<District>();
+
+    }
+
+
+
     class District
     {
         // district name
+        public string DistrictName { get; set;}
+
         // list of stores
+        //List<Stores> ListOfStores { get; set; } = new List<Stores>();
+
         // add store/district
+
+
+
         // add district manager
+
+
         // I made changes
     }
 }
